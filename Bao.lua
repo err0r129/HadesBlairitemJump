ab23608ca4508fda7b8e36d1a435c201240ff3a2
LuaErr0r129 <157980495+err0r129@users.noreply.github.com>
2024-03-28 01:34:49 -0300

    Bao.lua

--- Bao.lua
+++ Bao.lua
@@ -1,7 +1,9 @@
- --Made By Raspyredstoner
+--Made By Raspyredstoner
 mouse = game.Players.LocalPlayer:GetMouse()
 tool = Instance.new("Tool")
 tool.RequiresHandle = false
 tool.Name = "err0r129 Jump"
 tool.Activated:connect(function()
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
